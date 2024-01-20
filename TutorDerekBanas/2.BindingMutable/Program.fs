ee76a7944050c5839b774166af0316669f6d7443
zzfima <zzfima@gmail.com>
2024-01-20 14:39:26 +0200

    add 2.BindingMutable

---
+++ TutorDerekBanas/2.BindingMutable/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let bind_stuff = 
+    let mutable weight = 1
+    weight <- 2
+    Console.WriteLine($"Weight = {weight}")
+
+    let change_me = ref 3
+    change_me := 4
+    Console.WriteLine($"change_me = {!change_me}")
+    Console.WriteLine($"change_me = {change_me.Value}")
+
+
+let _ = Console.ReadLine()
