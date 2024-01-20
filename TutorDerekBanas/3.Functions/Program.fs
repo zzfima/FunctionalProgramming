8f847bc0abc1344aadced27d8626584d904a358f
zzfima <zzfima@gmail.com>
2024-01-20 15:19:19 +0200

    add 3.Functions

---
+++ TutorDerekBanas/3.Functions/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let do_func() =
+    let sum (a, b) =  
+        a + b
+    Console.WriteLine($"5 + 4 = {sum(5, 4)}")
+do_func()
+
+let rec factorial x =
+    if x < 1 then 1
+    else x * factorial(x - 1)
+
+Console.WriteLine($"factorial(5) = {factorial 5}")
+
+let _ = Console.ReadLine()
