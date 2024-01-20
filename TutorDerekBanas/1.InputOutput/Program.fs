d1da748eced6b0a135e04578b998d2d5c3d5a831
zzfima <zzfima@gmail.com>
2024-01-20 13:22:05 +0200

    Input Output

---
+++ TutorDerekBanas/1.InputOutput/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let hello = 
+    printfn "please enter your name"
+    let name = Console.ReadLine()
+    printfn $"hello, {name}!"
+
+    printfn $"PI = %.4f{Math.PI}"
+    printfn $"PI = %.10f{Math.PI}"
+    printfn $"PI = %M{(decimal)Math.PI}"
+    let big_pi = 3.141592653589793238462643383M
+    printfn $"PI = %M{big_pi}"
+
+hello
+Console.ReadLine() |> ignore
