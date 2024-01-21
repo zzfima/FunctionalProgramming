debc6af6b5128d23ca6fb8bb1c9de8fabb9adc83
Efim Zabarsky <EZ>
2024-01-21 13:21:42 +0200

    add pipeline

--- TutorDerekBanas/3.Functions/Program.fs
+++ TutorDerekBanas/3.Functions/Program.fs
@@ -11,10 +11,17 @@ let rec factorial x =
     else x * factorial(x - 1)
 Console.WriteLine($"factorial(4) = {factorial 4}")
 
-let lists() =
+let listMap() =
     let single_list = [1;2;3;]
     let list_double = List.map (fun x -> x * 2) single_list
     Console.WriteLine($"single list = {single_list}, double list = {list_double}")
-lists()
+listMap()
+
+let pipeline() =
+    [5;6;7;8]
+    |> List.filter(fun v -> v % 2 = 0)
+    |> List.map(fun v -> v * 2)
+    |> Console.WriteLine
+pipeline()
 
 let _ = Console.ReadLine()
