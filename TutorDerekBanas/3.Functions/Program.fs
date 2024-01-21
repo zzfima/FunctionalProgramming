f1c32d0ec11482de27bfe308f2627ab1d9b00c8d
Efim Zabarsky <EZ>
2024-01-21 13:00:20 +0200

    add list map

--- TutorDerekBanas/3.Functions/Program.fs
+++ TutorDerekBanas/3.Functions/Program.fs
@@ -9,7 +9,12 @@ do_func()
 let rec factorial x =
     if x < 1 then 1
     else x * factorial(x - 1)
+Console.WriteLine($"factorial(4) = {factorial 4}")
 
-Console.WriteLine($"factorial(5) = {factorial 5}")
+let lists() =
+    let single_list = [1;2;3;]
+    let list_double = List.map (fun x -> x * 2) single_list
+    Console.WriteLine($"single list = {single_list}, double list = {list_double}")
+lists()
 
 let _ = Console.ReadLine()
