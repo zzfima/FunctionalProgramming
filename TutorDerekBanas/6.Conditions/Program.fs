74a7487000913f84a0f34e6020d4aa7d5ec7950f
Efim Zabarsky <EZ>
2024-01-22 10:09:53 +0200

    add conditions

---
+++ TutorDerekBanas/6.Conditions/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let print_age age = 
+    if age < 10 then
+        Console.WriteLine("is a kid")
+    elif age < 24 then
+        Console.WriteLine("is a young")
+    else
+        Console.WriteLine("is a big")
+
+[1;11;33] |> List.iter(fun a -> print_age(a))
+
+Console.ReadLine() |> ignore
